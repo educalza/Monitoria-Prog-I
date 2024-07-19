79181348f40dfc5a9275e86825d9dec3129de592
Eduardo <eduardoalvescalza20@gmail.com>
2024-07-19 19:41:31 -0300

    First Commit

---
+++ p2/ex2.c
@@ -0,0 +1,6 @@
+/* Em um estacionamento rotativo, o cidadão pode parar seu veículo por até 2 horas no local,
+pagando uma tarifa mínima de R$5,00. Caso o cidadão exceda o horário permitido, é cobrado
+um adicional de R$3,00 por hora ou fração excedida. Faça programa que receba como entrada a
+informação de vários veículos, incluindo sua placa e o número de horas que ele ficou no local;
+Em seguida, o programa deve exibir: a) a placa de cada veículo, o número de horas estacionado e a
+tarifa paga devidamente calculada; e b) o total arrecadado.*/
